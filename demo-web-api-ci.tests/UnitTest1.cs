68feb140a47d93d5e39c3f33021a04a320b7ca34
Frank J Hughes <frank.j.hughes@outlook.com>
2026-02-24 12:10:36 -0800

    Add Test Project

---
+++ demo-web-api-ci.tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace demo_web_api_ci.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.NotNull(null);
+        }
+    }
+}
